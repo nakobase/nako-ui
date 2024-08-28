620c0469ec6f053ac82871a109765afb38c69063
nakobase <nakobase7@gmail.com>
2024-08-28 10:59:22 +0900

    chore: generate type files

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/lib/index.ts'),
-      name: 'index',
       fileName: 'index',
+      name: 'Nako UI',
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
@@ -22,4 +21,12 @@ export default defineConfig({
       },
     },
   },
+  plugins: [
+    react(),
+    dts({
+      exclude: ['src/lib/**/*.stories.tsx'],
+      include: ['src/lib/**/*', 'src/vite-env.d.ts'],
+      tsconfigPath: resolve(__dirname, 'tsconfig.app.json'),
+    }),
+  ],
 });
