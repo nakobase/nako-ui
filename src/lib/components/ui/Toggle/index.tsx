88b843d3a4857fb43fe8b437b1fa7da911ae4d57
nakobase <nakobase7@gmail.com>
2024-12-23 16:52:46 +0900

    feat: add Toggle story

--- src/lib/components/ui/Toggle/index.tsx
+++ src/lib/components/ui/Toggle/index.tsx
@@ -7,7 +7,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/utils';
 
 const toggleVariants = cva(
-  'nako-inline-flex nako-items-center nako-justify-center nako-rounded-md nako-text-sm nako-font-medium nako-ring-offset-background nako-transition-colors hover:nako-bg-muted hover:nako-text-muted-foreground focus-visible:nako-outline-none focus-visible:nako-ring-2 focus-visible:nako-ring-ring focus-visible:nako-ring-offset-2 disabled:nako-pointer-events-none disabled:nako-opacity-50 data-[state=on]:nako-bg-accent data-[state=on]:nako-text-accent-foreground',
+  'nako-inline-flex nako-items-center nako-justify-center nako-rounded-md nako-text-sm nako-font-medium nako-ring-offset-background nako-transition-colors hover:nako-bg-muted hover:nako-text-muted-foreground focus-visible:nako-outline-none focus-visible:nako-ring-2 focus-visible:nako-ring-ring focus-visible:nako-ring-offset-2 disabled:nako-pointer-events-none disabled:nako-opacity-50 data-[state=on]:nako-bg-accent data-[state=on]:nako-text-accent-foreground [&_svg]:nako-pointer-events-none [&_svg]:nako-size-4 [&_svg]:nako-shrink-0 nako-gap-2',
   {
     defaultVariants: {
       size: 'default',
@@ -15,9 +15,9 @@ const toggleVariants = cva(
     },
     variants: {
       size: {
-        default: 'nako-h-10 nako-px-3',
-        lg: 'nako-h-11 nako-px-5',
-        sm: 'nako-h-9 nako-px-2.5',
+        default: 'nako-h-10 nako-px-3 nako-min-w-10',
+        lg: 'nako-h-11 nako-px-5 nako-min-w-11',
+        sm: 'nako-h-9 nako-px-2.5 nako-min-w-9',
       },
       variant: {
         default: 'nako-bg-transparent',
